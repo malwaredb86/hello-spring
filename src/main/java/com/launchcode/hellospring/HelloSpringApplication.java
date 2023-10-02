a8030bf76769be931c354c5130fe389dd6a7dc3f
malwaredb86 <mdurham86@gmail.com>
2023-10-02 16:27:39 -0400

    Finished intro to templates up to thymeleaf forms

--- src/main/java/com/launchcode/hellospring/HelloSpringApplication.java
+++ src/main/java/com/launchcode/hellospring/HelloSpringApplication.java
@@ -2,7 +2,11 @@ package com.launchcode.hellospring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+@Controller
+@ResponseBody
 @SpringBootApplication
 public class HelloSpringApplication {
 
